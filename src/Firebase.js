393f85ae9f4e4cf3e2c06d24eb82568c928b49f1
Sunil Besra <sunilbesra28@gmail.com>
2023-09-06 13:08:52 +0530

    changes done

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwp9V5aNBu5E0SsLH1y4qJrLL8QWUKGhM",
-  authDomain: "clone-6abe8.firebaseapp.com",
-  projectId: "clone-6abe8",
-  storageBucket: "clone-6abe8.appspot.com",
-  messagingSenderId: "752575321294",
-  appId: "1:752575321294:web:061c2c5c5aeed11253ec9f"
+  apiKey: "AIzaSyBj-FAvJlXZ8SDcq5lAaUbsetIb8k-WsZ4",
+  authDomain: "facebook-clone-12e1d.firebaseapp.com",
+  projectId: "facebook-clone-12e1d",
+  storageBucket: "facebook-clone-12e1d.appspot.com",
+  messagingSenderId: "653074113523",
+  appId: "1:653074113523:web:99d8a827d15d6ab52ea886"
 };
 
 // Initialize Firebase
